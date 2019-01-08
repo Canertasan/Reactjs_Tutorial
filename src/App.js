8a0f526b80f130e0d2a691c37cb57ee5e2039e7a
Canertasan <canertasan@sabanciuniv.edu>
2019-01-08 17:21:17 +0300

    Exercise one!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    // Increment function
+    // then you dont need to bind sth. On constructor.
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    //console.log(counter);
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    // then you dont need to bind sth. On constructor.
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    // then you dont need to bind sth. On constructor.
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    // then you dont need to bind sth. On constructor.
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value !== 0).length} // Please and please god why did i write lenght like lenght!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        />
+        <main className="contanier">
+          <Counters
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,64 +1,47 @@
 import React, { Component } from "react";
 
+const space = {
+  margin: "0px 0px 0px 7px"
+};
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  handleIncrement = product => {
-    // then you dont need to bind sth. On constructor.
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()} </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
-        </button>{" "}
-      </React.Fragment>
+          +
+        </button>
+        <button
+          style={space}
+          disabled={!this.props.counter.value}
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          x
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
 export default Counter;
-
-{
-  /*
-        
-
-        {this.state.tags.length === 0 && "please create a new tag!"}
-        {this.renderTags()}
-           */
-}
-
-/*
-  
-
-    renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-  }*/
